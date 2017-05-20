d90d10e0ab3ed8262a829a5f23dafb752e73a4ca
nmakes <nav.naveenvenkat@gmail.com>
2017-05-20 10:13:44 +0530

    adding commented basic header file

--- switch-ds/switch.h
+++ switch-ds/switch.h
@@ -4,31 +4,58 @@
 
 using namespace std;
 
+// NEED TO IMPLEMENT ERROR AND EXCEPTION HANDLING
+
 enum route { mesh_left, mesh_down, mesh_right, mesh_up };
 
 class ComputeUnit
 {
 	private:
-	int reg;
+	char reg;			// a 1 Byte register
 
 	public:
-	int setR(int x)
+	int setR(char x)	// sets the register value
 	{
 		reg = x;
+		return 1;
 	}
-	void function()
+
+	char getR()
+	{
+		return reg;
+	}
+	
+	void function()	// implement this as a variadic function with one argument as a function pointer
 	{
 		cout << reg++;
 	}
-	ComputeUnit()
+
+	ComputeUnit()	// implement constructors as necessary
 	{
-		reg=0;
+		reg='A';
 	}
 };
 
+/*
+	The architecture using a mesh switch is as follows:
+
+	0--0--1--1--0--0--1--0
+	|  |  |  |  |  |  |  |
+	1--0--0--1--0--1--1--0
+	|  |  |  |  |  |  |  |
+	0--0--0--0--1--0--1--1
+	|  |  |  |  |  |  |  |
+	1--1--1--0--1--0--0--1
+
+	Each digit represents a node. 0 represents a switch, 1 represents a compute unit.
+
+	Compute units can do computations ("smart node"), while switches can only transfer data from one node to another ("dumb nodes").
+
+*/
+
 struct mesh_switch // node of a 4 way mesh
 {
-	int hasCU;
+	int hasCU;		// if true, then it can do computations
 	int reg;
 	ComputeUnit * cu = NULL;
 	mesh_switch * left;
