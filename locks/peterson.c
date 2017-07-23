c45212364993c6affe840029297e67e9c07ee66b
nmakes <nav.naveenvenkat@gmail.com>
2017-07-23 12:24:10 +0100

    commented peterson lock

--- locks/peterson.c
+++ locks/peterson.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 
-void petersonLock(int thread_id, int &turn, int * flag)
-{
-	flag[thread_id] = 1;
-	turn = 1 - thread_id; // 1 if 0, 0 if 1
+/*
+	This implementation of Peterson lock is applicable 
+	for two threads. When one thread requests a lock, it
+	marks itself as a requesting thread (in flag array),
+	and lets the other thread execute. If the other thread
+	has to execute, it waits, otherwise it executes its
+	critical section.
+*/
 
-	while(flag[turn]==1 && turn==1-thread_id);
+void petersonLock(int thread_id, int *turn, int flag[])
+{
+	// mark as lock requesting thread
+	flag[thread_id] = 1;
+
+	// let the other thread execute first
+	*turn = 1 - thread_id; // 1 if 0, 0 if 1
+
+	// if the other thread is in its critical section, wait
+	while(flag[*turn]==1 && *turn==1-thread_id);
 
 	// critical section code here
+	printf("Critical section of thread %d", thread_id);
 
+	// unmark lock request
 	flag[thread_id] = 0;
 }
